49b75cb0a0c617bddde6d20892e84e3f16e0398f
Mohan Liu <mohan.liu.personal@gmail.com>
2017-04-26 17:26:05 -0400

    fix links

--- wp-content/themes/wordpress-bootstrap-master/modal.php
+++ wp-content/themes/wordpress-bootstrap-master/modal.php
@@ -24,7 +24,7 @@ Template Name: Modal
               <!-- <div class="modal-header" style="border-bottom:0px">
                 <button id="closeModal" type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
               </div> -->
-              <img width="50" height="50" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/expand.png" onclick="opencusmodal()">
+              <img width="50" height="50" src="/hartnett/wp-content/themes/wordpress-bootstrap-master/hartnett/expand.png" onclick="opencusmodal()">
             </div>
           </div>
         </div>
