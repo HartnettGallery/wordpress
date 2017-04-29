ba1dee783b93ac86e348735f54ee533408b94c7d
Mohan Liu <mohan.liu.personal@gmail.com>
2017-04-29 11:04:28 -0400

    floor plan is done

--- wp-content/themes/wordpress-bootstrap-master/floorplan.php
+++ wp-content/themes/wordpress-bootstrap-master/floorplan.php
@@ -8,15 +8,27 @@ Template Name: Floorplan
 
       <div id="content" class="clearfix row" style="margin-top:10vh; margin-bottom:10vh">
 
-        <div id="main" class="clearfix" role="main" style="padding-right:20rem;padding-left:10rem; border:0px red solid">
 
         <div class="your-class">
-          <div>your content</div>
-          <div>your content</div>
-          <div>your content</div>
+          <div><img class = "floorplan" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/FloorPlanGallery/HartnettModel1.jpg" /></div>
+          <div><img class = "floorplan" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/FloorPlanGallery/HartnettModel2.jpg" /></div>
+          <div><img class = "floorplan" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/FloorPlanGallery/HartnettModel3.jpg" /></div>
+          <div><img class = "floorplan" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/FloorPlanGallery/HartnettModel4.jpg" /></div>
+          <div><img class = "floorplan" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/FloorPlanGallery/HartnettFloorPlan.jpg" /></div>
         </div>
 
+        <p> &nbsp </p>
+        <p> &nbsp </p>
+
+        <div class="multiple-items">
+          <div><img class = "floorplan_multiitem" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/FloorPlanGallery/HartnettModel1.jpg" /></div>
+          <div><img class = "floorplan_multiitem" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/FloorPlanGallery/HartnettModel2.jpg" /></div>
+          <div><img class = "floorplan_multiitem" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/FloorPlanGallery/HartnettModel3.jpg" /></div>
+          <div><img class = "floorplan_multiitem" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/FloorPlanGallery/HartnettModel4.jpg" /></div>
+          <div><img class = "floorplan_multiitem" src="/wp-content/themes/wordpress-bootstrap-master/hartnett/FloorPlanGallery/HartnettFloorPlan.jpg" /></div>
         </div>
+
+
 
       </div>
 <style>
@@ -24,6 +36,7 @@ body{
   background-image: url('/wp-content/themes/wordpress-bootstrap-master/hartnett/proposal_old.svg');
   height:100vh;
   background-size: 100%;
+
 }
 .single-item{
   border: 1px red solid;
@@ -32,6 +45,32 @@ body{
   text-align: center;
   color: #419be0;
   background: white;
+}
+.your-class {
+  /*border:1px red solid;*/
+  width:auto;
+  height:100%;
+}
+
+.floorplan {
+  width: 100%;
+  max-height: 50vh;
+  margin: auto;
+  display: block;
+}
+.multiple-items {
+
+  margin-left: 0vw;
+  width:100%;
+}
+
+.floorplan_multiitem {
+  padding:10px;
+  width: 100%;
+  height: auto;
+  max-height: 20vh;
+  margin: auto;
+  display: block;
 }
 </style>
 
