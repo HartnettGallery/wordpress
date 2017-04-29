264e6a0ba48453c7e1b88ec99029e1fe045d5f02
Mohan Liu <mohan.liu.personal@gmail.com>
2017-04-29 10:08:32 -0400

    fix

---
+++ wp-content/themes/wordpress-bootstrap-master/floorplan.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Template Name: Floorplan
+*/
+?>
+
+<?php get_header(); ?>
+
+      <div id="content" class="clearfix row" style="margin-top:10vh; margin-bottom:10vh">
+
+        <div id="main" class="clearfix" role="main" style="padding-right:20rem;padding-left:10rem; border:0px red solid">
+
+        <div class="your-class">
+          <div>your content</div>
+          <div>your content</div>
+          <div>your content</div>
+        </div>
+
+        </div>
+
+      </div>
+<style>
+body{
+  background-image: url('/wp-content/themes/wordpress-bootstrap-master/hartnett/proposal_old.svg');
+  height:100vh;
+  background-size: 100%;
+}
+.single-item{
+  border: 1px red solid;
+}
+.slick-slide {
+  text-align: center;
+  color: #419be0;
+  background: white;
+}
+</style>
+
+<?php get_footer(); ?>
